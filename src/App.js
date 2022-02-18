a84f26722ffc83f9443a4738bb20cfb1ecaefff2
RSobrinho <rafarrsobrinho@hotmail.com>
2022-02-18 19:49:57 -0400

    feat: Configuracoes principais e retangulo inicial criados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './style.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <div className="rectangle"></div>
+    </div>
+  )
 }
 
 export default App
