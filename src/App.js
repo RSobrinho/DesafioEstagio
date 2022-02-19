7fc02f1d00a1f309028c79da0dd6126b7720014c
RSobrinho <rafarrsobrinho@hotmail.com>
2022-02-19 13:46:48 -0400

    fix: organizacao HTML nos containers do header, e dos graficos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './style.css'
 import Header from './Components/Header'
 import BarChart from './Components/BarChart'
+import ScatterChart from './Components/ScatterChart'
 function App() {
   return (
     <div className="App">
       <div className="rectangle"></div>
 
-      <div className="container">
+      <div className="Header__container">
         <Header />
+      </div>
+      <div className="Charts__container">
         <BarChart />
+        <ScatterChart />
       </div>
     </div>
   )
